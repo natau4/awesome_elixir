f9b9a40db18aa942c44718608ee882ade204e7cb
Azat Zinnyurov <natau4@gmail.com>
2018-05-11 02:55:27 +0300

    [+] Main page with lib's list was developed

--- lib/awesome_elixir_web/controllers/page_controller.ex
+++ lib/awesome_elixir_web/controllers/page_controller.ex
@@ -1,7 +1,40 @@
 defmodule AwesomeElixirWeb.PageController do
+  import Ecto.Query
+
   use AwesomeElixirWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, params) do
+    c_query = from(
+                    c in AwesomeElixir.Category, 
+                    join: a in AwesomeElixir.Lib, 
+                    on: c.id == a.category_id, 
+                    group_by: c.id,
+                    order_by: c.name
+                  )
+    a_query = from(
+                    a in AwesomeElixir.Lib,
+                    order_by: a.name
+                  )
+    min_stars = validate_min_stars(params["min_stars"])
+
+    if min_stars > 0 do
+      c_query = c_query |> where([a], fragment("stars >= ?", ^min_stars))
+      a_query = a_query |> where([a], a.stars >= ^min_stars)
+    end
+
+    categories = c_query |> AwesomeElixir.Repo.all
+    applications = a_query |> AwesomeElixir.Repo.all
+    render conn, "index.html", categories: categories, applications: applications
+  end
+
+  defp validate_min_stars(min_stars) do
+    case min_stars do
+      nil -> 0
+      _ ->
+        case Integer.parse(min_stars) do
+          {int, ""} -> int
+          _ -> 0
+        end
+    end
   end
 end
